d973963dada4e1ade56504d3310c93c1444b18be
yokabLle <yokablle@gmail.com>
2016-11-14 02:43:20 +0300

    add less

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var livereload = require('gulp-livereload');
 var includer = require('gulp-htmlincluder');
+var less = require('gulp-less');
 
 
 // ---------------------------Connect-----------------
@@ -15,20 +16,28 @@ gulp.task('connect', function() {
 
 
 // --------------------------Tasks-------------------
+// -----html
 gulp.task('htmlIncluder', function() {
     gulp.src('develop/**/*.html')
     	.pipe(includer())
         .pipe(gulp.dest('build/'))
         .pipe(connect.reload());
 });
- 
+// -----Less
+gulp.task('less', function () {
+  gulp.src('develop/less/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('build/css/'))
+    .pipe(connect.reload());
+});
+
 
 // ---------------------------Watch------------------
 gulp.task('watch', function () {
   gulp.watch(['develop/**/*.html'], ['htmlIncluder']);
+  gulp.watch(['develop/less/**/*.less'], ['less']);
 });
 
 
-
 // ------------------------Main Task-----------------
-gulp.task('default', ['htmlIncluder', 'connect', 'watch']);
+gulp.task('default', ['htmlIncluder', 'less', 'connect', 'watch']);
